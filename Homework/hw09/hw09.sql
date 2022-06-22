886e88a76612249e4ae366b94b3b84b0d0390c4d
lemorage <one.lemorage@gmail.com>
2022-06-22 15:49:41 +0100

    HW09

---
+++ Homework/hw09/hw09.sql
@@ -0,0 +1,54 @@
+CREATE TABLE parents AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+
+CREATE TABLE dogs AS
+  SELECT "abraham" AS name, "long" AS fur, 26 AS height UNION
+  SELECT "barack"         , "short"      , 52           UNION
+  SELECT "clinton"        , "long"       , 47           UNION
+  SELECT "delano"         , "long"       , 46           UNION
+  SELECT "eisenhower"     , "short"      , 35           UNION
+  SELECT "fillmore"       , "curly"      , 32           UNION
+  SELECT "grover"         , "short"      , 28           UNION
+  SELECT "herbert"        , "curly"      , 31;
+
+CREATE TABLE sizes AS
+  SELECT "toy" AS size, 24 AS min, 28 AS max UNION
+  SELECT "mini"       , 28       , 35        UNION
+  SELECT "medium"     , 35       , 45        UNION
+  SELECT "standard"   , 45       , 60;
+
+
+-- The size of each dog
+CREATE TABLE size_of_dogs AS
+  SELECT name AS name, size AS size
+    FROM dogs, sizes
+    WHERE min < height AND height <= max;
+
+
+-- All dogs with parents ordered by decreasing height of their parent
+CREATE TABLE by_parent_height AS
+  SELECT child
+    FROM parents, dogs
+    WHERE parent = name
+    ORDER BY -height;
+
+
+-- Filling out this helper table is optional
+CREATE TABLE siblings AS
+  SELECT a.child AS a, s1.size AS a_size, b.child AS b, s2.size AS b_size
+    FROM parents AS a, parents AS b, size_of_dogs AS s1, size_of_dogs AS s2
+    WHERE a.parent = b.parent AND a.child < b.child
+      AND a.child = s1.name AND b.child = s2.name;
+
+-- Sentences about siblings that are the same size
+CREATE TABLE sentences AS
+  SELECT "The two siblings, " || a || " plus " || b || " have the same size: " || a_size
+    FROM siblings
+    WHERE a_size = b_size;
+
